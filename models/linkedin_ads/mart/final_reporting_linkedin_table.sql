19e46e4669d61301832d566c574ffd11c2633ff3
Nimra Amer <nimra.amer@thedatumlabs.com>
2025-07-24 13:46:32 +0000

    m

--- models/linkedin_ads/mart/final_reporting_linkedin_table.sql
+++ models/linkedin_ads/mart/final_reporting_linkedin_table.sql
@@ -1,4 +1,4 @@
 SELECT creative_name, campaign_name, month_ AS Month, total_cost_local AS total_cost,
-  total_clicks, total_impressions, cpc AS CPC, ctr AS CTR
+  total_clicks, total_impressions, cpc AS CPC, ctr AS CTR , (cpc*total_clicks) AS total_spent
 
 FROM {{ ref('linkedin_ads_inter') }}
