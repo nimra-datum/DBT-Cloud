c0d1d81706e868d903d3ee539bcb31f084f45b59
Nimra Amer <nimra.amer@thedatumlabs.com>
2025-07-29 05:47:42 +0000

    updated structure

--- models/mart/final_mart.sql
+++ models/mart/final_mart.sql
@@ -1,10 +1,10 @@
 SELECT 'facebook' AS platform,ad_name, adset_name AS AD_Group_SET_Creative, campaign_name, date, status, total_impressions, total_clicks, total_spend, CPC,CTR
-FROM {{ref('final_reporting_facebook_table')}}
+FROM {{ref('facebook_ads_fact')}}
 
 UNION
 SELECT 'google' AS platform, ad_name, ad_group_name AS AD_Group_SET_Creative ,campaign_name, date, status, total_impressions, total_clicks , total_spend, CPC ,CTR
-FROM {{ ref('final_repoting_google_table') }}
+FROM {{ ref('google_ads_fact') }}
 
 UNION
 SELECT 'linkedin' AS platform, 'N/A' AS ad_name, creative_name AS AD_Group_SET_Creative, campaign_name, date, status,total_impressions, total_clicks, total_spend, cpc AS CPC, ctr AS CTR
-FROM {{ ref('final_reporting_linkedin_table') }}
+FROM {{ ref('linkedin_ads_inter') }}
--- models/mart/final_reporting_facebook_table.sql
+++
@@ -1,2 +0,0 @@
-SELECT ad_name, adset_name, campaign_name, date, status, total_impressions, total_clicks, total_spend, CPC,CTR
-FROM {{ref('facebook_ads_fact')}}
